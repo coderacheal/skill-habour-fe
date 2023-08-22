e600786b8944e86184d8d40955d6187d952c2e0a
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-08-22 12:54:43 +0000

    Add socials component to project

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/skill-habour-logo.png';
+import Socials from './Socials';
 
 const HomePage = () => (
   <div className="homePage">
     <div className="dashboard">
       <img src={logo} alt="Skill Share Logo" className="skill-share-logo" />
-      <div>
+      <div className="dashboardNavigations">
         <h2>Dashboard</h2>
         <ul className="navigation">
-          <Link to="/home">Courses</Link>
-          <Link to="/reserve">Reserve Course</Link>
-          <Link to="/reservation/my_reservation">My Reservations</Link>
-          <Link to="/reservation/delete_reservation">Remove Reservation</Link>
+          <Link to="/home" className="links">Courses</Link>
+          <Link to="/reservations" className="links">Reserve Course</Link>
+          <Link to="/reservations/my_reservation" className="links">My Reservations</Link>
+          <Link to="/reservations/delete_reservation" className="links">Delete Reservation</Link>
         </ul>
       </div>
+      <Socials />
     </div>
     <div className="availableClasses" />
   </div>
