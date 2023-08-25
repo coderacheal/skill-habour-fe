df4268b2f5c358e8afbf760f6f0fbf6712319f37
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-25 12:23:26 +0500

    Link reservation form to main page

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import TestRideForm from './TestRideForm';
+import '../styles/TestRideForm.css'; // Assuming this is the component you've created for the form
 
-const Reservations = () => (
-  <div className="wrapper">
-    <Dashboard />
-  </div>
-);
+const Reservations = () => {
+  const location = useLocation();
+  const autofillUsername = location.state?.autofillUsername || '';
+
+  return (
+    <div className="wrapper">
+      <Dashboard />
+      <div className="content">
+        <h2 className="title">Reserve your course</h2>
+        <TestRideForm autofillUsername={autofillUsername} />
+      </div>
+    </div>
+  );
+};
 
 export default Reservations;
