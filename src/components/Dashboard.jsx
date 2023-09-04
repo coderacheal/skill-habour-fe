e622e7b1be0ee82e73f79d37940c46b41132cf91
Ouail-01 <ouaillaarabi@gmail.com>
2023-09-04 22:56:57 +0100

    Fix the linters

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React, { useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import logo from '../assets/skill-habour-logo.png';
 import Socials from './Socials';
-import {FaBars, FaTimes} from "react-icons/fa";
 
 const Dashboard = () => {
   const user = useSelector((store) => store.auth.user);
@@ -12,8 +12,8 @@ const Dashboard = () => {
 
   const navRef = useRef();
   const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
-  }
+    navRef.current.classList.toggle('responsive_nav');
+  };
 
   return (
     <div className="dashboard">
@@ -36,15 +36,15 @@ const Dashboard = () => {
           {loggedInUser && <Link to="/my_reservations" className={`links ${(pathname === '/my_reservations') ? 'active' : ''}`}>My Reservervations</Link>}
           <Link to="/add_course" className={` links ${(pathname === '/add_course') ? 'active' : ''}`}>Add Course</Link>
           <Link to="/delete_reservation" className={`links ${(pathname === '/delete_reservation') ? 'active' : ''}`}>Delete Reservation</Link>
-          <button className='nav-btn nav-close-btn' onClick={showNavbar}>
-            <FaTimes/>
+          <button className="nav-btn nav-close-btn" type="button" onClick={showNavbar}>
+            <FaTimes />
           </button>
         </ul>
         <Socials />
       </div>
-      
-      <button className='nav-btn nav-open-btn' onClick={showNavbar}>
-        <FaBars/>
+
+      <button className="nav-btn nav-open-btn" type="button" onClick={showNavbar}>
+        <FaBars />
       </button>
     </div>
   );
