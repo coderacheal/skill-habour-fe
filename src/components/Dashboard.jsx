7c81ccabcbb5fef5acef2bdef08fd0faff06d342
Ouail-01 <ouaillaarabi@gmail.com>
2023-09-04 13:50:30 +0100

    Add the icons

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector} from 'react-redux';
 import logo from '../assets/skill-habour-logo.png';
 import Socials from './Socials';
+import {FaBars, FaTimes} from "react-icons/fa";
 
 const Dashboard = () => {
   const user = useSelector((store) => store.auth.user);
   const { pathname } = useLocation();
 
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
   return (
     <div className="dashboard">
       <img src={logo} alt="Skill Share Logo" className="skill-share-logo" />
-      <div className="dashboardNavigations">
+      <div className="dashboardNavigations" ref={navRef}>
         <h2>Dashboard</h2>
         {
           user ? (
@@ -29,9 +35,16 @@ const Dashboard = () => {
           <Link to="/my_reservations" className={` links ${(pathname === '/my_reservations') ? 'active' : ''}`}>My Reservations</Link>
           <Link to="/add_course" className={` links ${(pathname === '/add_course') ? 'active' : ''}`}>Add Course</Link>
           <Link to="/delete_reservation" className={`links ${(pathname === '/delete_reservation') ? 'active' : ''}`}>Delete Reservation</Link>
+          <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+            <FaTimes/>
+          </button>
         </ul>
+        <Socials />
       </div>
-      <Socials />
+      
+      <button className='nav-btn nav-open-btn' onClick={showNavbar}>
+        <FaBars/>
+      </button>
     </div>
   );
 };
