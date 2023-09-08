686ae0aa60e36cbaa9eb44e71f06de95d1ae65a5
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-09-08 19:21:44 +0500

    Update app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import AddCourse from './components/AddCourse';
 import DeleteCourse from './components/DeleteCourse';
 
 const App = () => (
-  <Router basename="/skill-habour">
+  <Router>
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/courses" element={<HomePage />} />
